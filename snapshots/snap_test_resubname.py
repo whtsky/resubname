e0dbc84d39fd0730e5ecb875461ee1efd09b80c8
Wu Haotian <whtsky@gmail.com>
2019-10-28 22:07:00 +0800

    ci: use docker image for testing

--- snapshots/snap_test_resubname.py
+++ snapshots/snap_test_resubname.py
@@ -8,29 +8,36 @@ from snapshottest import Snapshot
 snapshots = Snapshot()
 
 snapshots['test_exclude 1'] = [
-    '[ANK-Raws] それでも町は廻っている 02 (BDrip 1920x1080 HEVC-YUV420P10 FLAC).ass',
-    '[ANK-Raws] それでも町は廻っている 02 (BDrip 1920x1080 HEVC-YUV420P10 FLAC).mkv',
-    '[ANK-Raws] それでも町は廻っている (Ep_05 Creditless ED) (BDrip 1920x1080 HEVC-YUV420P10 FLAC).mkv',
-    '[ANK-Raws] それでも町は廻っている 01 (BDrip 1920x1080 HEVC-YUV420P10 FLAC).ass',
-    '[ANK-Raws] それでも町は廻っている 01 (BDrip 1920x1080 HEVC-YUV420P10 FLAC).mkv'
+    '[ANK-Raws] それでも町は廻っている (Ep_05 Creditless ED) (BDrip 1920x1080 HEVC-YUV420P10 FLAC).mkv',
+    '[ANK-Raws] それでも町は廻っている 01 (BDrip 1920x1080 HEVC-YUV420P10 FLAC).ass',
+    '[ANK-Raws] それでも町は廻っている 01 (BDrip 1920x1080 HEVC-YUV420P10 FLAC).mkv',
+    '[ANK-Raws] それでも町は廻っている 02 (BDrip 1920x1080 HEVC-YUV420P10 FLAC).ass',
+    '[ANK-Raws] それでも町は廻っている 02 (BDrip 1920x1080 HEVC-YUV420P10 FLAC).mkv'
 ]
 
-snapshots['test_rename 1'] = [
-    '[VCB-Studio] GIRLS und PANZER [03][Ma10p_1080p][x265_flac].ass',
-    '[VCB-Studio] GIRLS und PANZER [03][Ma10p_1080p][x265_flac].mkv',
-    '[VCB-Studio] GIRLS und PANZER [05.5][Ma10p_1080p][x265_flac].ass',
-    '[VCB-Studio] GIRLS und PANZER [05][Ma10p_1080p][x265_flac].mkv',
-    '[VCB-Studio] GIRLS und PANZER [05][Ma10p_1080p][x265_flac].ass',
-    '[VCB-Studio] GIRLS und PANZER [05.5][Ma10p_1080p][x265_flac].mkv'
-]
-
-snapshots['test_dryrun 1'] = '''
-/03.ass -> /[VCB-Studio] GIRLS und PANZER [03][Ma10p_1080p][x265_flac].ass
+snapshots['test_dryrun 1'] = '''/03.ass -> /[VCB-Studio] GIRLS und PANZER [03][Ma10p_1080p][x265_flac].ass
 /05.5.ass -> /[VCB-Studio] GIRLS und PANZER [05.5][Ma10p_1080p][x265_flac].ass
 /05.ass -> /[VCB-Studio] GIRLS und PANZER [05][Ma10p_1080p][x265_flac].ass
 '''
 
-snapshots['test_exclude 2'] = '''
-/[ANE] Soredemo Machi wa Mawatte Iru - EP01 [BD 1920x1080 H.264 FLAC].CASO-SC.ass -> /[ANK-Raws] それでも町は廻っている 01 (BDrip 1920x1080 HEVC-YUV420P10 FLAC).ass
-/[ANE] Soredemo Machi wa Mawatte Iru - EP02 [BD 1920x1080 H.264 FLAC].CASO-SC.ass -> /[ANK-Raws] それでも町は廻っている 02 (BDrip 1920x1080 HEVC-YUV420P10 FLAC).ass
+snapshots['test_exclude 2'] = '''/[ANE] Soredemo Machi wa Mawatte Iru - EP01 [BD 1920x1080 H.264 FLAC].CASO-SC.ass -> /[ANK-Raws] それでも町は廻っている 01 (BDrip 1920x1080 HEVC-YUV420P10 FLAC).ass
+/[ANE] Soredemo Machi wa Mawatte Iru - EP02 [BD 1920x1080 H.264 FLAC].CASO-SC.ass -> /[ANK-Raws] それでも町は廻っている 02 (BDrip 1920x1080 HEVC-YUV420P10 FLAC).ass
 '''
+
+snapshots['test_rename 1'] = [
+    '03.ass',
+    '05.5.ass',
+    '05.ass',
+    '[VCB-Studio] GIRLS und PANZER [03][Ma10p_1080p][x265_flac].mkv',
+    '[VCB-Studio] GIRLS und PANZER [05.5][Ma10p_1080p][x265_flac].mkv',
+    '[VCB-Studio] GIRLS und PANZER [05][Ma10p_1080p][x265_flac].mkv'
+]
+
+snapshots['test_rename 2'] = [
+    '[VCB-Studio] GIRLS und PANZER [03][Ma10p_1080p][x265_flac].ass',
+    '[VCB-Studio] GIRLS und PANZER [03][Ma10p_1080p][x265_flac].mkv',
+    '[VCB-Studio] GIRLS und PANZER [05.5][Ma10p_1080p][x265_flac].ass',
+    '[VCB-Studio] GIRLS und PANZER [05.5][Ma10p_1080p][x265_flac].mkv',
+    '[VCB-Studio] GIRLS und PANZER [05][Ma10p_1080p][x265_flac].ass',
+    '[VCB-Studio] GIRLS und PANZER [05][Ma10p_1080p][x265_flac].mkv'
+]
--- test_resubname.py
+++ test_resubname.py
@@ -9,26 +9,36 @@ from resubname import cli
 FIXTURES_PATH = Path(__file__).parent / "fixtures"
 
 
+def sorted_glob(p: Path, pattern="*"):
+    """
+    return sorted filenames for matched files.
+    """
+    names = [x.name for x in p.glob(pattern)]
+    names.sort()
+    return names
+
+
 def copy_fixtures(src: Path, tmp_path: Path) -> Path:
     fixture_path = tmp_path / uuid4().hex
     shutil.copytree(src, fixture_path)
     return fixture_path
 
 
-def test_rename(tmp_path: Path, snapshot):
+def test_rename(capsys, tmp_path: Path, snapshot):
     fixtures = copy_fixtures(FIXTURES_PATH / "basic", tmp_path)
-    files_before = [x.name for x in fixtures.glob("*")]
+    files_before = sorted_glob(fixtures)
+    snapshot.assert_match(files_before)
     cli([str(fixtures)])
-    files_after = [x.name for x in fixtures.glob("*")]
+    files_after = sorted_glob(fixtures)
     assert files_after != files_before
     snapshot.assert_match(files_after)
 
 
 def test_dryrun(capsys, tmp_path: Path, snapshot):
     fixtures = copy_fixtures(FIXTURES_PATH / "basic", tmp_path)
-    files_before = [x.name for x in fixtures.glob("*")]
+    files_before = sorted_glob(fixtures)
     cli([str(fixtures), "--dryrun"])
-    files_after = [x.name for x in fixtures.glob("*")]
+    files_after = sorted_glob(fixtures)
     assert files_after == files_before
 
     out, err = capsys.readouterr()
@@ -43,12 +53,11 @@ def test_raise_when_subtitle_and_videos_number_dismatch():
 
 def test_exclude(capsys, tmp_path: Path, snapshot):
     fixtures = copy_fixtures(FIXTURES_PATH / "exclude", tmp_path)
-    files_before = [x.name for x in fixtures.glob("*")]
+    files_before = sorted_glob(fixtures)
     cli([str(fixtures), "-e", "creditless"])
-    files_after = [x.name for x in fixtures.glob("*")]
+    files_after = sorted_glob(fixtures)
     assert files_after != files_before
     snapshot.assert_match(files_after)
 
     out, err = capsys.readouterr()
     snapshot.assert_match(out.replace(str(fixtures.resolve()), ""))
-
